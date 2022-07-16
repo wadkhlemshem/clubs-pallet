ba9f2f2c34913586aca6f7b704075baa2f4b62f9
Johanan Wahlang <johanan@mlabs.city>
2022-07-16 18:57:17 +0530

    Add call to create club

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,8 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+        /// Club created.
+		ClubCreated { club_number: u32 },
         /// User added to club.
 		UserAddedToClub { club_number: u32, user: T::AccountId },
 		/// User removed from club.
@@ -46,6 +48,8 @@ pub mod pallet {
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
+		/// Club already exists.
+		ClubAlreadyExists,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -53,5 +57,22 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+        /// Create a new club.
+		#[pallet::weight(50_000_000)]
+		pub fn create_club(
+			origin: OriginFor<T>,
+			club_number: u32,
+		) -> DispatchResult {
+			ensure_root(origin)?;
+			match Clubs::<T>::get(club_number) {
+				Some(_) => return Err(Error::<T>::ClubAlreadyExists.into()),
+				None => {
+					let club: BoundedBTreeSet<T::AccountId, T::MaxClubSize> = BoundedBTreeSet::new();
+					<Clubs<T>>::insert(&club_number, club);
+				},
+			};
+			Self::deposit_event(Event::<T>::ClubCreated { club_number });
+			Ok(())
+		}
 	}
 }
